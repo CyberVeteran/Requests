39b63b27b84e9fcd54834559b7b40c8bf75fbe34
vasyl.kliushta <vasyl.kliushta@proton.me>
2023-04-13 01:18:22 +0300

    using await and its over

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-fetch('db.json').then((response) => {
+const  getRequest = async () => {
+    const  response =await fetch('dbb.json')
+    if(response.status!==200){
+        throw new Error('cannot fetch data')
+    }
+    const data = await response.json()
+    const drivers = data.drivers
+    const blogs = data.blogs
+    //console.log(blogs, drivers)
+    return {drivers,blogs}
+
+}
+getRequest()
+    .then(({blogs,drivers})=>{
+        console.log('resolved')
+        console.log('drivers',drivers)
+        console.log('blogs',blogs)
+
+    })
+    .catch(err=>{console.log('rejected', err.message)})
+
+
+/*fetch('db.json').then((response) => {
     console.log('resolved', response)
     return response.json()
 }).then((data) => {
@@ -6,4 +28,4 @@ fetch('db.json').then((response) => {
     console.log(data?.drivers, data?.blogs)
 }).catch((error) => {
     console.log('rejected', error)
-})
+}) */
