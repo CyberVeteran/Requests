df9932ca66ede7305a93b9b0a9cbb632c57765de
vasyl.kliushta <vasyl.kliushta@proton.me>
2023-04-12 19:02:03 +0300

    using fetch

--- index.js
+++ index.js
@@ -1,43 +1,9 @@
-/*
-*/
-const getRequest = (dataType) => {
-    return new Promise((resolve, reject) => {
-        const request = new XMLHttpRequest();
+fetch('db.json').then((response) => {
+    console.log('resolved', response)
+    return response.json()
+}).then((data) => {
 
-        request.addEventListener('readystatechange', () => {
-
-            if (request.readyState === 4) {
-                if (request.status === 200) {
-                    const data = JSON.parse(request.responseText);
-                    if (dataType in data) {
-                        resolve(data[dataType]);
-
-                    } else {
-                        reject(`Property '${dataType}' not found in data`);
-                    }
-                } else {
-                    reject(`Error ${request.status} while retrieving data`);
-                }
-            }
-        });
-
-        request.open('GET', 'db.json');
-
-        request.send();
-
-    });
-
-};
-
-getRequest('drivers')
-    .then((drivers) => {
-        console.log('Promise resolved', drivers);
-        return getRequest('blogs');
-    })
-    .then((data) => {
-        console.log('Second promise resolved', data);
-        
-    })
-    .catch((error) => {
-        console.log('Promise rejected', error);
-    });
+    console.log(data?.drivers, data?.blogs)
+}).catch((error) => {
+    console.log('rejected', error)
+})
